b9612b21c4dee051340c8039ad36cd82b94c9446
Dolph Mathews <dolph.mathews@gmail.com>
2014-02-24 07:32:59 -0600

    add reminder about port-forwarding after devstack result

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
 sudo apt-get install git -y
 git clone git://github.com/openstack-dev/devstack.git
 cd devstack; ./stack.sh
+echo "Horizon is being port-forwarded to http://127.0.0.1:8000"
